e77134671a6d44f2c02818438cdd6e5074f77c1c
SOunit <workbookwork7@gmail.com>
2022-03-17 14:50:10 -0700

    add decorator factory sample

--- 08_decorator/src/app.ts
+++ 08_decorator/src/app.ts
@@ -1,9 +1,23 @@
-function Logger(constructor: Function) {
-  console.log("Logging...");
-  console.log(constructor);
+function Logger(logString: string) {
+  return (constructor: Function) => {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
-@Logger
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const p = new constructor();
+    const hookEl = document.getElementById(hookId);
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+// @Logger("LOGGING - PERSON")
+@WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
   name = "Max";
 
