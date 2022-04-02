5cd94ef385820da7db5de7c176476ee03e20c7b6
SOunit <workbookwork7@gmail.com>
2022-04-02 12:23:25 -0700

    add node ts sample

--- 15_node_express/src/app.ts
+++ 15_node_express/src/app.ts
@@ -1,1 +1,18 @@
-console.log("something");
+// typescript import syntax for type support
+import express, { NextFunction, Request, Response } from "express";
+import todoRoutes from "./routes/todos";
+import { json } from "body-parser";
+
+const app = express();
+
+app.use(json());
+
+app.use("/todos", todoRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
+
+app.listen(5000, () => {
+  console.log("listen");
+});
